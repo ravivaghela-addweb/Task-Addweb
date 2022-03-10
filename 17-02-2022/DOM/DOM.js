68677f5c00fe66e8666db7613af5bc8d6bebbc72
ravivaghela-addweb <ravi.s.addweb@gmail.com>
2022-03-11 02:44:08 +0530

    JavaScript - Array, Methods, DOM, RegEx, Rest & Spread Operator

---
+++ 17-02-2022/DOM/DOM.js
@@ -0,0 +1,42 @@
+var element;
+
+element = document
+console.log(element);
+console.log(element=document);
+
+// element = document.all;
+// console.log(element);
+
+// element = document.documentElement;
+// console.log(element);
+
+// element = document.head;
+// console.log(element);
+
+// element = document.title;
+// console.log(element);
+
+// element = document.images;
+// console.log(element);
+
+// element = document.anchors;
+// console.log(element);
+
+// element = document.links;
+// console.log(element);
+
+// element = document.forms;
+// console.log(element);
+
+// element = document.doctype;
+// console.log(element);
+
+// element = document.URL;
+// console.log(element);
+
+// element = document.baseURI;
+// console.log(element);
+
+// element = document.domain;
+// console.log(element);
+
